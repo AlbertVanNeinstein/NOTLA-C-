c35c4d14eda205c04fdbf295e01600a70b5ef98f
Will Saint <ipwnyou2010@gmail.com>
2016-05-09 11:29:11 -0400

    Added Item Creation base

--- NOTLA C++/NOTLA C++/NOTLA C++.cpp
+++ NOTLA C++/NOTLA C++/NOTLA C++.cpp
@@ -2,10 +2,15 @@
 //
 
 #include "stdafx.h"
-
-
+#include <iostream>
+using namespace std;
+// Item Creation; Order of values: Name, Flavor text, Type, Attack, Defense, Speed, Health, Special
+int it1[8] {1, 2, 1, 5, 2, 0, 0, 0};
 int main()
 {
+	if (it1[1] == 2)
+		cout << "Hello" << endl;
+	cout << it1[1] << endl;
     return 0;
 }
 
